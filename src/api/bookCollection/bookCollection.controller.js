392da5bc22539ff9d1f4bd4a0fb99ccbf5024c21
Friosn <franz.rios.nino@gmail.com>
2022-11-21 18:48:19 +0100

    Controllers done!

--- src/api/bookCollection/bookCollection.controller.js
+++ src/api/bookCollection/bookCollection.controller.js
@@ -1,1 +1,35 @@
 const Collection = require("./bookCollection.model");
+
+const setError = require("../../helper/error/handle.error");
+
+const getCollection = async (req, res, next) => {
+  try {
+    const collection = await Collection.find()
+      .populate("toread")
+      .populate("read");
+    return res.json({
+      status: 200,
+      message: "All collections recovered! 📚",
+      data: { collection },
+    });
+  } catch (error) {
+    return next(setError(500, "Failiure recovering Collections 🍂"));
+  }
+};
+
+const postCollection = async (req, res, next) => {
+  try {
+    const newCollection = new Collection(req.body);
+    const collectionToDB = await newCollection.save();
+
+    return res.json({
+      status: 500,
+      message: "New Collections added! 🤓",
+      data: { collectionToDB },
+    });
+  } catch (error) {
+    return next(setError(500, "Imposible to post Collections right now 😵‍💫"));
+  }
+};
+
+module.exports = { getCollection, postCollection };
--- src/api/readBooks/readBooks.controller.js
+++ src/api/readBooks/readBooks.controller.js
@@ -1,1 +1,33 @@
 const ReadBooks = require("./readBooks.mode");
+
+const setError = require("../../helper/error/handle.error");
+
+const getRead = async (req, res, next) => {
+  try {
+    const read = await ReadBooks.find();
+    return res.json({
+      status: 200,
+      message: "All Read books recovered! 📚",
+      data: { read },
+    });
+  } catch (error) {
+    return next(setError(500, "Failiure recovering the read books 🍂"));
+  }
+};
+
+const postRead = async (req, res, next) => {
+  try {
+    const newRead = new ReadBooks(req.body);
+    const readToDB = await newRead.save();
+
+    return res.json({
+      status: 500,
+      message: "New Book to Read added! 🤓",
+      data: { readToDB },
+    });
+  } catch (error) {
+    return next(setError(500, "Imposible to post right now 😵‍💫"));
+  }
+};
+
+module.exports = { getRead, postRead };
