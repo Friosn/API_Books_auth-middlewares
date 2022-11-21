7976eb0f80ed1c387e66b33fff3a5594a95ce21b
Friosn <franz.rios.nino@gmail.com>
2022-11-21 14:42:06 +0100

    user.model added, encription made

--- src/api/user/user.model.js
+++ src/api/user/user.model.js
@@ -1,2 +1,19 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    emoji: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//now we'll preSave the password hashing it  with the number of encriptions asked
+userSchema.pre("save", function (next) {
+  this.password = bcrypt.hashSync(this.password, 10);
+  next();
+});
